c0300ca5d8c8ec4cd83714a60b81e92c39cebcb8
indev <bdmitry450@gmail.com>
2022-07-09 21:52:14 +0300

    Delete copy and add destructors to Event

--- src/event/event.hpp
+++ src/event/event.hpp
@@ -1,10 +1,19 @@
 #ifndef EVENT_HPP
 #define EVENT_HPP
 
-#include <string>
+#include <memory>
+
 #include "../entity/entity.hpp"
+#include "../item/item_def.hpp"
 
 namespace Terrarium {
+
+    struct ItemEvent {
+        std::shared_ptr<ItemDef> item_def;
+        std::weak_ptr<Entity> user;
+
+        sf::Vector2f position;
+    };
 
     struct Event {
         enum Type {
@@ -18,11 +27,28 @@ namespace Terrarium {
         } type;
 
         union {
-            struct {
-                std::string item_id;
-                Entity *user = nullptr;
-            } item;
+            ItemEvent *item = nullptr;
+            // ...more events data
         };
+
+        // Delete copy because i don't know how union with smart pointers inside
+        // would behave.
+        Event(const Event &copy) = delete;
+        Event &operator=(const Event &copy) = delete;
+
+        ~Event() {
+            switch (type) {
+                case ItemUseStart:
+                case ItemUseStop:
+                case ItemAltUseStart:
+                case ItemAltUseStop:
+                case ItemSelect:
+                {
+                    delete item;
+                }
+                break;
+            }
+        }
     };
 
 }
