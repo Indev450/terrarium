b9c12bc630b594b3104622d338910bbe427cc5b0
indev <bdmitry450@gmail.com>
2022-07-17 19:56:34 +0300

    Regiser "block item" with block

--- wrappers/block.lua
+++ wrappers/block.lua
@@ -1,19 +1,48 @@
 terrarium.registered_blocks = {}
 terrarium.block_names = {} -- lookup table to get block name by id
 
+local function add_block_item(name, def)
+    function def.on_use(user, itemstack, position, use_ctx)
+        local x = math.floor(position.x)
+        local y = math.floor(position.y)
+
+        if core._get_block(x, y) == 0 then
+            core._set_block(x, y, def.block_id)
+        end
+    end
+
+    function def.on_alt_use(user, itemstack, position, use_ctx)
+        local x = math.floor(position.x)
+        local y = math.floor(position.y)
+
+        if core._get_wall(x, y) == 0 then
+            core._set_wall(x, y, def.block_id)
+        end
+    end
+
+    def.inventory_image = def.inventory_image or def.image
+
+    terrarium.register_item(name, def)
+end
+
 local block_defaults = {
     image = "",
     slippery = 0,
     is_solid = true,
+
+    description = "Block",
+    max_items = 999,
 }
 
 function terrarium.register_block(name, def)
     apply_defaults(def, block_defaults)
 
-    def.id = core._register_block(def)
+    def.block_id = core._register_block(def)
 
     terrarium.registered_blocks[name] = def
-    terrarium.block_names[def.id] = name
+    terrarium.block_names[def.block_id] = name
+
+    add_block_item(name, def)
 end
 
 function terrarium.register_block_alias(name, alias)
@@ -27,7 +56,7 @@ end
 function terrarium.get_block_id(name)
     if terrarium.registered_blocks[name] == nil then return 0 end
 
-    return terrarium.registered_blocks[name].id
+    return terrarium.registered_blocks[name].block_id
 end
 
 function terrarium.get_tile(x, y)
