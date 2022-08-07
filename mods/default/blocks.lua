aff7c9bbded7a87f3b0dc05631a161810a86fddd
indev <bdmitry450@gmail.com>
2022-08-07 14:08:07 +0300

    If player breaks chest, they get items inside of it

--- mods/default/blocks.lua
+++ mods/default/blocks.lua
@@ -60,6 +60,21 @@ terrarium.register_block("default:chest", {
     on_interact = function(position, user)
         user:open_inventory_ui(terrarium.get_block_inventory(position.x, position.y), 8, 4)
     end,
+
+    on_destroy = function(position, user)
+        if user ~= nil and user.ref.is_player then
+            local inventory = terrarium.get_block_inventory(position.x, position.y)
+            local user_inventory = user.ref:get_player_inventory()
+
+            for i = 0, (8*4 - 1) do
+                user_inventory:add_item(inventory:get(i))
+            end
+
+            return true
+        end
+
+        return false
+    end,
 })
 
 terrarium.register_block_alias("default:stone", "mapgen:filler_block")
