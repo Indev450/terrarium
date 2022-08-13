451a2c71152508efba62078f74946dabaeac8385
indev <bdmitry450@gmail.com>
2022-08-13 15:00:59 +0300

    Remove "energy" bar (no need to test bars anymore)

--- mods/default/player.lua
+++ mods/default/player.lua
@@ -13,25 +13,5 @@ terrarium.register_on_player_join(function(player)
     player.ref:get_player_inventory():get(0):set("default:copper_pickaxe", 1)
     player.ref:get_player_inventory():get(1):set("default:chest", 5)
 
-    player:add_bar("energy", {
-        max_value = 20,
-
-        bar_color = color.new(10, 255, 20),
-
-        text_style = "Division",
-
-        text_color = color.new(40, 10, 150),
-
-        text = "Energy: ",
-
-        geometry = {
-            x = 600,
-            y = 38,
-
-            width = 128,
-            height = 32,
-        }
-    })
-
     player:set_bar_value("energy", 15)
 end)
