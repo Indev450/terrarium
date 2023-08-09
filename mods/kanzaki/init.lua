88700a44b3de82f7bbee87d6823bd4e523362667
Indev <bdmitry450@gmail.com>
2023-08-09 18:54:52 +0300

    kanzaki...

---
+++ mods/kanzaki/init.lua
@@ -0,0 +1,16 @@
+terrarium.register_block("kanzaki:blue_bricks", {
+    description = "Blue bricks\n\"Kanzaki...\"",
+
+    draw_type = "Autotile",
+    image = "blue_bricks.png",
+
+    slippery = 0.2,
+
+    is_solid = true,
+
+    groups = {
+        ["ground"] = 1,
+    },
+
+    inventory_image = "blue_bricks_item.png",
+})
