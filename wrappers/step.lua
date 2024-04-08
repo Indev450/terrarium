c4521fa462148a9390b3eeee9bb2ed907a9ef279
Indev <bdmitry450@gmail.com>
2024-04-08 13:03:11 +0300

    Add 'call_on_start' argument to terrarium.register_on_step_timed() lua function

--- wrappers/step.lua
+++ wrappers/step.lua
@@ -4,12 +4,14 @@ function terrarium.register_on_step(fn)
     table.insert(core._on_step, fn)
 end
 
-function terrarium.register_on_step_timed(time, fn)
+function terrarium.register_on_step_timed(time, fn, call_on_start)
     local t = timer.new(time, true)
 
     terrarium.register_on_step(function(dtime)
         if t:tick(dtime) then fn() end
     end)
+
+    if call_on_start then t:tick(time) end
 end
 
 core._update_hooks["step"] = function(dtime)
